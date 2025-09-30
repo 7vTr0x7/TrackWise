57f23f39288a676a3ee7af6f6421702c0c6fc562
7vTr0x7 <7vtrox7@gmail.com>
2025-09-30 18:25:38 +0530

    middleware fixes

--- middleware.js
+++ middleware.js
@@ -1,26 +1,43 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// Only protect these routes
+// Protected routes
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
   "/transaction(.*)",
 ]);
 
+// Arcjet middleware
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "DRY_RUN", // Use DRY_RUN first to reduce bundle size
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+
+// Clerk middleware
 const clerk = clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
-
   if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
-
   return NextResponse.next();
 });
 
-export default clerk;
+// Combine both
+export default createMiddleware(aj, clerk);
 
+// Run on all routes except static files, but always on APIs
 export const config = {
-  matcher: ["/dashboard(.*)", "/account(.*)", "/transaction(.*)"],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
