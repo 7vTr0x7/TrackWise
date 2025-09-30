9abe999a4277d43586d7b924e7170f28c645389a
7vTr0x7 <7vtrox7@gmail.com>
2025-09-30 17:01:25 +0530

    middleware fixes

--- middleware.js
+++ middleware.js
@@ -19,10 +19,5 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: [
-    // Only protect sensitive routes with Clerk
-    "/dashboard(.*)",
-    "/account(.*)",
-    "/transaction(.*)",
-  ],
+  matcher: ["/dashboard(.*)", "/account(.*)", "/transaction(.*)"],
 };
