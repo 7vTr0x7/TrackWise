f6e1cedf35a9e9fa81d081a3381e3c8d98bce23d
7vTr0x7 <7vtrox7@gmail.com>
2025-09-30 19:02:58 +0530

    middleware fixes

--- middleware/index.js
+++ middleware/index.js
@@ -3,10 +3,3 @@ import clerkMiddleware from "./clerk";
 import { createMiddleware } from "@arcjet/next";
 
 export default createMiddleware(arcjetMiddleware, clerkMiddleware);
-
-export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
-};
