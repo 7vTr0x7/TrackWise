a7f5d1a9f1e012ab984e9748e6cbfc5dc7597ea9
7vTr0x7 <7vtrox7@gmail.com>
2025-09-30 17:48:59 +0530

    middleware fixes

--- middleware.js
+++ middleware.js
@@ -1,22 +1,52 @@
+import arcjet, { detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
+// Match protected routes for Clerk
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
   "/transaction(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  const { userId, redirectToSignIn } = auth();
+// Arcjet middleware
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+
+// Clerk middleware
+const clerk = clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
   if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
-  return;
+
+  return NextResponse.next();
 });
+
+// Manual chaining to reduce size overhead
+export async function middleware(req) {
+  const arcjetResponse = await aj.middleware(req);
+  if (arcjetResponse) return arcjetResponse;
+
+  return clerk.middleware(req);
+}
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|.*\\.(?:css|js|png|jpg|svg|woff2?)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
+    "/dashboard(.*)",
+    "/account(.*)",
+    "/transaction(.*)",
   ],
 };
