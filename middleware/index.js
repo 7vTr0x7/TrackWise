e7c1336c4efa8bdd379e701aa54f7bf1afce0fbe
7vTr0x7 <7vtrox7@gmail.com>
2025-09-30 22:15:04 +0530

    middleware fixes

--- middleware/index.js
+++
@@ -1,5 +0,0 @@
-import arcjetMiddleware from "./arcjet";
-import clerkMiddleware from "./clerk";
-import { createMiddleware } from "@arcjet/next";
-
-export default createMiddleware(arcjetMiddleware, clerkMiddleware);
