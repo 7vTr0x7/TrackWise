17170a304c9a5c9bb1198e26d466dc8708fab3d9
7vTr0x7 <7vtrox7@gmail.com>
2025-09-30 18:55:11 +0530

    middleware fixes

--- middleware.js
+++ middleware.js
@@ -1,52 +1,9 @@
-import { NextResponse } from "next/server";
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import arcjetMiddleware from "./middleware/arcjet";
+import clerkMiddleware from "./middleware/clerk";
+import { createMiddleware } from "@arcjet/next";
 
-// Only import Arcjet dynamically to save bundle size
-let aj;
-async function getArcjet() {
-  if (!aj) {
-    const arcjetModule = await import("@arcjet/next");
-    aj = arcjetModule.default({
-      key: process.env.ARCJET_KEY,
-      rules: [
-        arcjetModule.shield({ mode: "DRY_RUN" }), // DRY_RUN reduces size
-        arcjetModule.detectBot({
-          mode: "DRY_RUN",
-          allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
-        }),
-      ],
-    });
-  }
-  return aj;
-}
+export default createMiddleware(arcjetMiddleware, clerkMiddleware);
 
-// Protected routes for Clerk
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/account(.*)",
-  "/transaction(.*)",
-]);
-
-// Clerk middleware
-const clerk = clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
-  if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
-    return redirectToSignIn();
-  }
-  return NextResponse.next();
-});
-
-// Combined middleware
-export default async function middleware(req) {
-  const arcjetMiddleware = await getArcjet();
-  const arcjetResponse = await arcjetMiddleware(req);
-  if (arcjetResponse) return arcjetResponse;
-
-  return clerk(req);
-}
-
-// Run on all routes except static assets, but always on APIs
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
---
+++ middleware/clerk.js
@@ -0,0 +1,21 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/account(.*)",
+  "/transaction(.*)",
+]);
+
+const clerk = clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
+  }
+
+  return NextResponse.next();
+});
+
+export default clerk;
